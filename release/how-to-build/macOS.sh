86675146940ff3b288b4210dcd1d44858fcb66df
octaspire <octaspire@octaspire.com>
2017-08-26 02:04:12 +0300

    Add missing file

---
+++ release/how-to-build/macOS.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env sh
+
+YELLOW="$(tput setaf 3 ; tput bold)"
+GREEN="$(tput setaf 2 ; tput bold)"
+NOCOLOR="$(tput setaf 9 ; tput sgr0)"
+
+echoAndRun() { echo "$@" ; "$@" ; }
+echoToDefs() { printf "$NOCOLOR\n" ; }
+
+CC=clang
+
+printf "$YELLOW\n"
+cat << EnDoFmEsSaGe
+1. Building stand alone unit test runner to test the release
+-------------------------------------------------------------------------------
+EnDoFmEsSaGe
+echoToDefs
+echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
+    -DOCTASPIRE_DERN_AMALGAMATED_UNIT_TEST_IMPLEMENTATION \
+    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
+    -DGREATEST_ENABLE_ANSI_COLORS                         \
+-I . octaspire-dern-amalgamated.c -lm                     \
+-o octaspire-dern-unit-test-runner
+
+
+
+printf "$YELLOW\n"
+cat << EnDoFmEsSaGe
+2. Building the embedding example
+-------------------------------------------------------------------------------
+EnDoFmEsSaGe
+echoToDefs
+echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
+    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
+-I . examples/embedding-example.c -lm                     \
+-o embedding-example
+
+
+printf "$YELLOW\n"
+cat << EnDoFmEsSaGe
+4. Building the interactive Dern REPL
+-------------------------------------------------------------------------------
+EnDoFmEsSaGe
+echoToDefs
+echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
+    -DOCTASPIRE_DERN_AMALGAMATED_REPL_IMPLEMENTATION      \
+    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
+-I . octaspire-dern-amalgamated.c -lm                     \
+-o octaspire-dern-repl
+
+
+
+printf "\nDone.\n$GREEN"
+echo   "=================================================================="
+echo   "Run programs and examples like this:"
+echo   "=================================================================="
+printf "%b1)%b ./octaspire-dern-unit-test-runner\n" $YELLOW $GREEN
+printf "%b2)%b ./embedding-example\n" $YELLOW $GREEN
+printf "%b4)%b ./octaspire-dern-repl -c\n" $YELLOW $GREEN
+echo "=================================================================="
+echoToDefs
+
