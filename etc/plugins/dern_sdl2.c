3ff6753e51356a9b396252297fd62b4d65188409
octaspire <octaspire@octaspire.com>
2017-11-07 21:13:27 +0200

    WIP

--- etc/plugins/dern_sdl2.c
+++ etc/plugins/dern_sdl2.c
@@ -19,12 +19,12 @@ octaspire_dern_value_t *dern_sdl2_create_window(
     size_t const stackLength = octaspire_dern_vm_get_stack_length(vm);
     size_t const numArgs = octaspire_dern_value_as_vector_get_length(arguments);
 
-    if (numArgs != 6)
+    if (numArgs < 5)
     {
         octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
         return octaspire_dern_vm_create_new_value_error_format(
             vm,
-            "Builtin 'sdl2-create-window' expects six arguments. "
+            "Builtin 'sdl2-create-window' expects at least five arguments. "
             "%zu arguments were given.",
             numArgs);
     }
@@ -130,14 +130,138 @@ octaspire_dern_value_t *dern_sdl2_create_window(
             octaspire_dern_value_helper_get_type_as_c_string(thirdArg->typeTag));
     }
 
-    // TODO XXX continue from here. Set w, h and flags.
+    octaspire_dern_value_t const * const fourthArg =
+        octaspire_dern_value_as_vector_get_element_at_const(arguments, 3);
+
+    octaspire_helpers_verify_not_null(fourthArg);
+
+    if (octaspire_dern_value_is_integer(fourthArg))
+    {
+        w = octaspire_dern_value_as_integer_get_value(fourthArg);
+    }
+    else
+    {
+        octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+        return octaspire_dern_vm_create_new_value_error_format(
+            vm,
+            "Builtin 'sdl2-create-window' expects integer as the fourth argument. "
+            "Type '%s' was given.",
+            octaspire_dern_value_helper_get_type_as_c_string(fourthArg->typeTag));
+    }
+
+    octaspire_dern_value_t const * const fifthArg =
+        octaspire_dern_value_as_vector_get_element_at_const(arguments, 4);
+
+    octaspire_helpers_verify_not_null(fifthArg);
+
+    if (octaspire_dern_value_is_integer(fifthArg))
+    {
+        h = octaspire_dern_value_as_integer_get_value(fifthArg);
+    }
+    else
+    {
+        octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+        return octaspire_dern_vm_create_new_value_error_format(
+            vm,
+            "Builtin 'sdl2-create-window' expects integer as the fifth argument. "
+            "Type '%s' was given.",
+            octaspire_dern_value_helper_get_type_as_c_string(fifthArg->typeTag));
+    }
+
+    Uint32 flags = 0;
+
+    for (size_t i = 5; i < numArgs; ++i)
+    {
+        octaspire_dern_value_t const * const flagArg =
+            octaspire_dern_value_as_vector_get_element_at_const(arguments, i);
+
+        octaspire_helpers_verify_not_null(flagArg);
+
+        if (octaspire_dern_value_is_symbol(flagArg))
+        {
+            if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "FULLSCREEN"))
+            {
+                flags |= SDL_WINDOW_FULLSCREEN;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "FULLSCREEN_DESKTOP"))
+            {
+                flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "OPENGL"))
+            {
+                flags |= SDL_WINDOW_OPENGL;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "HIDDEN"))
+            {
+                flags |= SDL_WINDOW_HIDDEN;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "BORDERLESS"))
+            {
+                flags |= SDL_WINDOW_BORDERLESS;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "RESIZABLE"))
+            {
+                flags |= SDL_WINDOW_RESIZABLE;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "MINIMIZED"))
+            {
+                flags |= SDL_WINDOW_MINIMIZED;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "MAXIMIZED"))
+            {
+                flags |= SDL_WINDOW_MAXIMIZED;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "INPUT_GRABBED"))
+            {
+                flags |= SDL_WINDOW_INPUT_GRABBED;
+            }
+            else if (octaspire_dern_value_as_symbol_is_equal_to_c_string(flagArg, "INPUT_HIGHDPI"))
+            {
+                flags |= SDL_WINDOW_INPUT_HIGHDPI;
+            }
+            else
+            {
+                octaspire_helpers_verify_true(
+                    stackLength == octaspire_dern_vm_get_stack_length(vm));
+
+                return octaspire_dern_vm_create_new_value_error_format(
+                    vm,
+                    "Builtin 'sdl2-create-window': unknown symbol '%s' as the %zu. argument. ",
+                    octaspire_dern_value_as_symbol_get_c_string(flagArg),
+                    i + 1);
+            }
+        }
+        else
+        {
+            octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+            return octaspire_dern_vm_create_new_value_error_format(
+                vm,
+                "Builtin 'sdl2-create-window' expects symbol as the %zu. argument. "
+                "Type '%s' was given.",
+                i + 1,
+                octaspire_dern_value_helper_get_type_as_c_string(flagArg->typeTag));
+        }
+    }
 
     SDL_Window * const window = SDL_CreateWindow(
         octaspire_dern_value_as_text_get_c_string(firstArg),
         x,
         y,
-        );
+        w,
+        h,
+        flags);
 
+    if (!window)
+    {
+        octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+        return octaspire_dern_vm_create_new_value_error_format(
+            vm,
+            "Builtin 'sdl2-create-window' failed to create a window. "
+            "Error message is: '%s'.",
+            SDL_GetError());
+    }
+
+    octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
     return octaspire_dern_vm_create_new_value_c_data(
         vm,
         DERN_SDL2_PLUGIN_NAME,
