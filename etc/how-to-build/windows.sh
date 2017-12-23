b43b27b2af989195bd8c1dd70d5266427da224c3
octaspire <octaspire@octaspire.com>
2017-12-23 15:35:14 +0200

    Lint and update build script for Microsoft Windows

--- etc/how-to-build/windows.sh
+++ etc/how-to-build/windows.sh
@@ -1,188 +1,111 @@
 #!/usr/bin/env sh
 
-YELLOW='\033[1;33m'
-GREEN='\033[1;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
 NOCOLOR='\033[0m'
 
 CC=gcc
 COVERAGE=""
 
-echoAndRun() { echo "$@" ; "$@" ; }
-echoToDefs() { printf "$NOCOLOR\n" ; }
+EXAMPLE_NAME=""
+EXAMPLE_ERROR_HINT=""
+EXAMPLE_SUCCESS_RUN=""
+
+echoAndRun() {
+    printf "%bBuilding %b$EXAMPLE_NAME...%b" "$BLUE" "$BOLD" "$NOCOLOR"
+
+    if ! "$@" > /dev/null 2>&1; then
+        printf "\n%bFAIL. POTENTIAL FIX: %b$EXAMPLE_ERROR_HINT\n"  "$RED" "$BOLD"
+    else
+        printf "%b\n" "$BLUE"
+        for i in "$@"
+        do
+            printf "%s " "$i"
+        done
+        printf "\n%bRUN WITH %b$EXAMPLE_SUCCESS_RUN\n" "$GREEN" "$BOLD"
+    fi
+    printf "%b\n" "$NOCOLOR"
+}
 
 if [ "$#" -ge "1" ]; then CC=$1; fi
 if [ "$2" = "--coverage" ]; then COVERAGE=$2; fi
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-1. Building stand alone unit test runner to test the release
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                      \
-    -DOCTASPIRE_DERN_AMALGAMATED_UNIT_TEST_IMPLEMENTATION      \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                     \
-    $COVERAGE                                                  \
-    -I . octaspire-dern-amalgamated.c -lm                      \
-    -lKernel32                                                 \
-    -o octaspire-dern-unit-test-runner
+EXAMPLE_NAME="stand alone unit test runner"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="octaspire-dern-unit-test-runner.exe"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_AMALGAMATED_UNIT_TEST_IMPLEMENTATION -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS $COVERAGE -I . octaspire-dern-amalgamated.c -lm -lKernel32 -o octaspire-dern-unit-test-runner
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-2. Building the embedding example
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                      \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                     \
-    -I . examples/embedding-example.c -lm                      \
-    -lKernel32                                                 \
-    -o embedding-example
+EXAMPLE_NAME="embedding example"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="embedding-example.exe"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS -I . examples/embedding-example.c -lm -lKernel32 -o embedding-example
+
+
+EXAMPLE_NAME="implementation library for binary library example"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN=""
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_AMALGAMATED_IMPLEMENTATION -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS octaspire-dern-amalgamated.c -lKernel32 -shared -Wl,--out-implib=imp.a
+EXAMPLE_NAME="binary library example"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl.exe examples\\use-mylib.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -o libmylib.dll examples/mylib.c -L . imp.a
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-3. Building the binary library example
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC                                                 \
-    -DOCTASPIRE_DERN_AMALGAMATED_IMPLEMENTATION                \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                     \
-    octaspire-dern-amalgamated.c                               \
-    -lKernel32                                                 \
-    -shared -Wl,--out-implib=imp.a
-
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c examples/mylib.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I .         \
-    -o libmylib.dll mylib.o -lKernel32 -L . imp.a
+EXAMPLE_NAME="interactive Dern REPL"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl.exe"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_AMALGAMATED_REPL_IMPLEMENTATION -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS -I . octaspire-dern-amalgamated.c -lm -lKernel32 -o octaspire-dern-repl
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-4. Building the interactive Dern REPL
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                      \
-    -DOCTASPIRE_DERN_AMALGAMATED_REPL_IMPLEMENTATION           \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                     \
-    -I . octaspire-dern-amalgamated.c -lm                      \
-    -lKernel32                                                 \
-    -o octaspire-dern-repl
+EXAMPLE_NAME="Dern socket plugin"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl examples\\irc-client.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -o libdern_socket.dll plugins/dern_socket.c -lws2_32 -L . imp.a
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-5. Building the 'dern_socket' (binary) plugin.
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_socket.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I .         \
-    -o libdern_socket.dll dern_socket.o -lws2_32 -L . imp.a
+EXAMPLE_NAME="Dern dir plugin"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl examples\\dern-dir-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -shared -o libdern_dir.dll plugins/dern_dir.c -lKernel32 -L . imp.a
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-6. Building the 'dern_dir' (binary) plugin.
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_dir.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I .         \
-    -o libdern_dir.dll dern_dir.o -lKernel32 -L . imp.a
+EXAMPLE_NAME="Dern ncurses plugin"
+read -rd '' EXAMPLE_ERROR_HINT << EOF
+    a) Go to https://pdcurses.sourceforge.io
+    b) Download file 'pdc34dllw.zip' into 'release'-directory
+    c) Run command: unzip pdc34dllw.zip
+    d) Run command: how-to-build/windows.sh
+EOF
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl examples\\dern-ncurses-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -I . -shared -o libdern_ncurses.dll plugins/dern_ncurses.c -L . -lpdcurses imp.a
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-7. Building the 'dern_ncurses' (binary) plugin.  PLEASE NOTE: This plugin
-   requires 'ncurses' library and headers to be installed on the system;
-   otherwise compilation will fail. Failure will not affect other steps,
-   so if this step fails and you don't want to use  binary plugin
-   'dern_ncurses', you don't have to do anything. Otherwise, download
-   'PDCurses' library for windows from:
-
-        a) Go to https://pdcurses.sourceforge.io
-        b) Download file 'pdc34dllw.zip' into 'release'-directory
-        c) Run command: unzip pdc34dllw.zip
-        d) Run command: how-to-build/windows.sh
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_ncurses.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I .         \
-    -o libdern_ncurses.dll dern_ncurses.o -lKernel32 -L . -lpdcurses imp.a
-
-
-
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-8. Building the 'dern_sdl2' (binary) plugin.  PLEASE NOTE: This plugin
-   requires 'sdl2' library and headers to be installed on the system;
-   otherwise compilation will fail. Failure will not affect other steps,
-   so if this step fails and you don't want to use  binary plugin
-   'dern_sdl2', you don't have to do anything. Otherwise, download and
-   use 'SDL2' library for windows by giving the following commands:
-
-        curl -O https://www.libsdl.org/release/SDL2-devel-2.0.7-mingw.tar.gz
-        tar xzf SDL2-devel-2.0.7-mingw.tar.gz
-        cp SDL2-2.0.7/i686-w64-mingw32/bin/SDL2.dll .
-        curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.2-mingw.tar.gz
-        tar xzf SDL2_image-devel-2.0.2-mingw.tar.gz
-        cp SDL2_image-2.0.2/i686-w64-mingw32/bin/*.dll .
-        curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.2-mingw.tar.gz
-        tar xzf SDL2_mixer-devel-2.0.2-mingw.tar.gz
-        cp SDL2_mixer-2.0.2/i686-w64-mingw32/bin/*.dll .
-        curl -O https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.14-mingw.tar.gz
-        tar xzf SDL2_ttf-devel-2.0.14-mingw.tar.gz
-        cp SDL2_ttf-2.0.14/i686-w64-mingw32/bin/*.dll .
-        how-to-build/windows.sh
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I .           \
-    -I SDL2-2.0.7/i686-w64-mingw32/include/SDL2                \
-    -I SDL2_image-2.0.2/i686-w64-mingw32/include/SDL2          \
-    -I SDL2_mixer-2.0.2/i686-w64-mingw32/include/SDL2          \
-    -I SDL2_ttf-2.0.14/i686-w64-mingw32/include/SDL2           \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                     \
-    -DOCTASPIRE_DERN_AMALGAMATED_IMPLEMENTATION                \
-    -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_IMAGE_LIBRARY         \
-    -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_MIXER_LIBRARY         \
-    -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_TTF_LIBRARY           \
--Dmain=SDL_main -c plugins/dern_sdl2.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I .         \
-    -o libdern_sdl2.dll dern_sdl2.o -L . imp.a                 \
-    -L SDL2-2.0.7/i686-w64-mingw32/lib -L SDL2_image-2.0.2/i686-w64-mingw32/lib \
-    -L SDL2_mixer-2.0.2/i686-w64-mingw32/lib -L SDL2_ttf-2.0.14/i686-w64-mingw32/lib \
-    -lSDL2 -lSDL2_image -lSDL2_mixer -lSDL2_ttf
-
-
-
-printf "\nDone.\n$GREEN"
-echo   "====================================================================="
-echo   "Run programs and examples like this using __Windows Command Prompt__:"
-echo   "====================================================================="
-printf "%b1)%b octaspire-dern-unit-test-runner.exe\n" $YELLOW $GREEN
-printf "%b2)%b embedding-example.exe\n" $YELLOW $GREEN
-printf '%b3)%b octaspire-dern-repl.exe examples\\use-mylib.dern\n' $YELLOW $GREEN
-printf "%b4)%b octaspire-dern-repl.exe\n" $YELLOW $GREEN
-printf "%b5)%b octaspire-dern-repl examples\\dern-sockets-echo-server.dern\n" $YELLOW $GREEN
-printf "%b+)%b octaspire-dern-repl examples\\dern-sockets-echo-client.dern\n" $YELLOW $GREEN
-printf "%b6)%b octaspire-dern-repl examples\\dern-dir-example.dern\n" $YELLOW $GREEN
-printf "%b7)%b octaspire-dern-repl examples\\dern-ncurses-example.dern\n" $YELLOW $GREEN
-printf "%b8)%b octaspire-dern-repl examples\\dern-sdl2-example.dern\n" $YELLOW $GREEN
-printf "%b9)%b octaspire-dern-repl examples\\irc-client.dern\n" $YELLOW $GREEN
-echo "======================================================================="
-echoToDefs
+EXAMPLE_NAME="Dern SDL2 plugin"
+read -rd '' EXAMPLE_ERROR_HINT << EOF
+    curl -O https://www.libsdl.org/release/SDL2-devel-2.0.7-mingw.tar.gz
+    tar xzf SDL2-devel-2.0.7-mingw.tar.gz
+    cp SDL2-2.0.7/i686-w64-mingw32/bin/SDL2.dll .
+    curl -O https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.2-mingw.tar.gz
+    tar xzf SDL2_image-devel-2.0.2-mingw.tar.gz
+    cp SDL2_image-2.0.2/i686-w64-mingw32/bin/*.dll .
+    curl -O https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-devel-2.0.2-mingw.tar.gz
+    tar xzf SDL2_mixer-devel-2.0.2-mingw.tar.gz
+    cp SDL2_mixer-2.0.2/i686-w64-mingw32/bin/*.dll .
+    curl -O https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-devel-2.0.14-mingw.tar.gz
+    tar xzf SDL2_ttf-devel-2.0.14-mingw.tar.gz
+    cp SDL2_ttf-2.0.14/i686-w64-mingw32/bin/*.dll .
+EOF
+EXAMPLE_SUCCESS_RUN="octaspire-dern-repl examples\\dern-sdl2-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_IMAGE_LIBRARY -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_MIXER_LIBRARY -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_TTF_LIBRARY -Dmain=SDL_main -I . -I SDL2-2.0.7/i686-w64-mingw32/include/SDL2 -I SDL2_image-2.0.2/i686-w64-mingw32/include/SDL2 -I SDL2_mixer-2.0.2/i686-w64-mingw32/include/SDL2 -I SDL2_ttf-2.0.14/i686-w64-mingw32/include/SDL2 -o libdern_sdl2.dll plugins/dern_sdl2.c -L . -L SDL2-2.0.7/i686-w64-mingw32/lib -L SDL2_image-2.0.2/i686-w64-mingw32/lib -L SDL2_mixer-2.0.2/i686-w64-mingw32/lib -L SDL2_ttf-2.0.14/i686-w64-mingw32/lib imp.a -lSDL2main -lSDL2 libSDL2_image.a -lSDL2_mixer -lSDL2_ttf
 
