2270262ffd9b98993fe312f5a04671e1189d7652
octaspire <octaspire@octaspire.com>
2017-12-24 20:03:56 +0200

    Lint and update build script for NetBSD

--- etc/how-to-build/NetBSD.sh
+++ etc/how-to-build/NetBSD.sh
@@ -1,159 +1,93 @@
 #!/usr/bin/env sh
 
-YELLOW="$(tput setaf 3 ; tput bold)"
-GREEN="$(tput setaf 2 ; tput bold)"
+BLUE="$(tput setaf 4)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+BOLD="$(tput bold)"
 NOCOLOR="$(tput setaf 9 ; tput sgr0)"
 
 CC=gcc
 COVERAGE=""
 
-echoAndRun() { echo "$@" ; "$@" ; }
-echoToDefs() { printf "$NOCOLOR\n" ; }
+EXAMPLE_NAME=""
+EXAMPLE_ERROR_HINT=""
+EXAMPLE_SUCCESS_RUN=""
+
+echoAndRun() {
+    printf "%bBuilding %b$EXAMPLE_NAME...%b" "$BLUE" "$BOLD" "$NOCOLOR"
+
+    if ! "$@" > /dev/null 2>&1; then
+        printf "\n%bFAIL. POTENTIAL FIX: %b$EXAMPLE_ERROR_HINT\n"  "$RED" "$BOLD"
+    else
+        printf "%b\n" "$BLUE"
+        for i in "$@"
+        do
+            printf "%s " "$i"
+        done
+        printf "\n%bRUN WITH %b$EXAMPLE_SUCCESS_RUN\n" "$GREEN" "$BOLD"
+    fi
+    printf "%b\n" "$NOCOLOR"
+}
 
 if [ "$#" -ge "1" ]; then CC=$1; fi
 if [ "$2" = "--coverage" ]; then COVERAGE=$2; fi
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-1. Building stand alone unit test runner to test the release
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
-    -DOCTASPIRE_DERN_AMALGAMATED_UNIT_TEST_IMPLEMENTATION \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
-    -DGREATEST_ENABLE_ANSI_COLORS                         \
-    $COVERAGE                                             \
--I . octaspire-dern-amalgamated.c -Wl,-export-dynamic -lm \
--o octaspire-dern-unit-test-runner
+EXAMPLE_NAME="stand alone unit test runner"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="./octaspire-dern-unit-test-runner"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_AMALGAMATED_UNIT_TEST_IMPLEMENTATION -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS -DGREATEST_ENABLE_ANSI_COLORS $COVERAGE -I . octaspire-dern-amalgamated.c -Wl,-export-dynamic -lm -o octaspire-dern-unit-test-runner
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-2. Building the embedding example
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
--I . examples/embedding-example.c -Wl,-export-dynamic -lm \
--o embedding-example
+EXAMPLE_NAME="embedding example"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="./embedding-example"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS -I . examples/embedding-example.c -Wl,-export-dynamic -lm -o embedding-example
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-3. Building the binary library example
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c examples/mylib.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I . -o libmylib.so mylib.o
+EXAMPLE_NAME="binary library example"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/use-mylib.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -o libmylib.so examples/mylib.c
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-4. Building the interactive Dern REPL
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra                 \
-    -DOCTASPIRE_DERN_AMALGAMATED_REPL_IMPLEMENTATION      \
-    -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS                \
--I . octaspire-dern-amalgamated.c -Wl,-export-dynamic -lm \
--o octaspire-dern-repl
+EXAMPLE_NAME="interactive Dern REPL"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="./octaspire-dern-repl -c"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_DERN_AMALGAMATED_REPL_IMPLEMENTATION -DOCTASPIRE_DERN_CONFIG_BINARY_PLUGINS -I . octaspire-dern-amalgamated.c -Wl,-export-dynamic -lm -o octaspire-dern-repl
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-5. Building the 'dern_socket' (binary) plugin.
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_socket.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I . -o libdern_socket.so dern_socket.o
+EXAMPLE_NAME="Dern socket plugin"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/irc-client.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -o libdern_socket.so plugins/dern_socket.c
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-6. Building the 'dern_dir' (binary) plugin.
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_dir.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I . -o libdern_dir.so dern_dir.o
+EXAMPLE_NAME="Dern dir plugin"
+EXAMPLE_ERROR_HINT="Install $CC compiler?"
+EXAMPLE_SUCCESS_RUN="LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-dir-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -I . -shared -o libdern_dir.so plugins/dern_dir.c
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-7. Building the 'dern_ncurses' (binary) plugin.  PLEASE NOTE: This plugin
-   requires development version of 'ncursesw' library (i.e. headers) to be
-   installed on the system; otherwise compilation will fail. Failure will
-   not affect other steps, so if this step fails and you don't want to use
-   binary plugin 'dern_ncurses', you don't have to do anything. Otherwise,
-   to install development version of library 'ncursesw':
-
-       - NetBSD: sudo pkgin install ncursesw
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_ncurses.c
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I . -o libdern_ncurses.so dern_ncurses.o -lcurses -lterminfo
+EXAMPLE_NAME="Dern ncurses plugin"
+EXAMPLE_ERROR_HINT="NetBSD: sudo pkgin install ncursesw"
+EXAMPLE_SUCCESS_RUN="LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-ncurses-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -I . -shared -o libdern_ncurses.so plugins/dern_ncurses.c -lcurses -lterminfo
 
 
 
-printf "$YELLOW\n"
-cat << EnDoFmEsSaGe
-8. Building the 'dern_sdl2' (binary) plugin.  PLEASE NOTE: This plugin
-   requires development version of 'SDL2' library (i.e. headers) to be
-   installed on the system; otherwise compilation will fail. Failure will
-   not affect other steps, so if this step fails and you don't want to use
-   binary plugin 'dern_sdl2', you don't have to do anything. Otherwise,
-   to install development version of library 'SDL2' manually:
-
-       - NetBSD: curl -O http://www.libsdl.org/release/SDL2-2.0.7.tar.gz
-                 tar zxf SDL2-2.0.7.tar.gz
-                 cd SDL2-2.0.7
-                 ./configure --disable-joystick       \\
-                             --disable-audio          \\
-                             --disable-x11-shared     \\
-                             --enable-directfb-shared
-                 gmake
-                 sudo gmake install
-
-    (If you have installed SDL2 with 'sudo pkgin install SDL2' you might
-    have to remove it first by running 'sudo pkgin remove SDL2' and then
-    install SDL2 manually. This is the way I was able to get SDL2 to work
-    in NetBSD at the moment.)
--------------------------------------------------------------------------------
-EnDoFmEsSaGe
-echoToDefs
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -fPIC -I . -c plugins/dern_sdl2.c `sdl2-config --cflags`
-echoAndRun $CC -O2 -std=c99 -Wall -Wextra -shared -I . -o libdern_sdl2.so dern_sdl2.o `sdl2-config --libs`
-
-
-
-printf "\nDone.\n$GREEN"
-echo   "=================================================================="
-echo   "Run programs and examples like this:"
-echo   "=================================================================="
-printf "%b1)%b ./octaspire-dern-unit-test-runner\n" $YELLOW $GREEN
-printf "%b2)%b ./embedding-example\n" $YELLOW $GREEN
-printf "%b3)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/use-mylib.dern\n" $YELLOW $GREEN
-printf "%b4)%b ./octaspire-dern-repl -c\n" $YELLOW $GREEN
-printf "%b5)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-sockets-echo-server.dern\n" $YELLOW $GREEN
-printf "%b+)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-sockets-echo-client.dern\n" $YELLOW $GREEN
-printf "%b6)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-dir-example.dern\n" $YELLOW $GREEN
-printf "%b7)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-ncurses-example.dern\n" $YELLOW $GREEN
-printf "%b8)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-sdl2-example.dern\n" $YELLOW $GREEN
-printf "%b9)%b LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/irc-client.dern\n" $YELLOW $GREEN
-echo "=================================================================="
-echoToDefs
+# SDL2 IMG and MIX libraries are currently disabled in this NetBSD build.
+# Those can be enabled with:
+# -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_IMAGE_LIBRARY -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_MIXER_LIBRARY
+EXAMPLE_NAME="Dern SDL2 plugin"
+EXAMPLE_ERROR_HINT="NetBSD: sudo pkgin install SDL2 SDL2_image SDL2_mixer SDL2_ttf"
+EXAMPLE_SUCCESS_RUN="LD_LIBRARY_PATH=. ./octaspire-dern-repl examples/dern-sdl2-example.dern"
+echoAndRun "$CC" -O2 -std=c99 -Wall -Wextra -fPIC -shared -DOCTASPIRE_DERN_AMALGAMATED_IMPLEMENTATION -DOCTASPIRE_DERN_SDL2_PLUGIN_USE_SDL_TTF_LIBRARY $(sdl2-config --cflags) -I . -o libdern_sdl2.so plugins/dern_sdl2.c $(sdl2-config --libs) -lSDL2_image -lSDL2_mixer -lSDL2_ttf
 
