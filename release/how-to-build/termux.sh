b2eaa3a92d171675778f79199bc7206e9c32cd72
octaspire <octaspire@octaspire.com>
2018-01-01 16:12:54 +0200

    Fix build script for Termux

--- release/how-to-build/termux.sh
+++ release/how-to-build/termux.sh
@@ -1,10 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/sh
 
-BLUE="$(tput setaf 4)"
-RED="$(tput setaf 1)"
-GREEN="$(tput setaf 2)"
-BOLD="$(tput bold)"
-NOCOLOR="$(tput setaf 9 ; tput sgr0)"
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD='\033[1m'
+NOCOLOR='\033[0m'
 
 CC=clang
 COVERAGE=""
