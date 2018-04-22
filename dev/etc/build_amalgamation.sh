465310b666520b16d7dc04a60ec331e4b523d4a5
octaspire <octaspire@octaspire.com>
2018-04-22 20:42:23 +0300

    Detect x86 and x86_64 Haiku from the Makefile, update submodules

--- dev/etc/build_amalgamation.sh
+++ dev/etc/build_amalgamation.sh
@@ -8,21 +8,25 @@ else
 fi
 
 if [ "$UNAME" = "Linux" ]; then
-    cd release && sh how-to-build/linux.sh        $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/linux.sh            $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "Darwin" ]; then
-    cd release && sh how-to-build/macOS.sh        $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/macOS.sh            $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "OpenBSD" ]; then
-    cd release && sh how-to-build/OpenBSD.sh      $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/OpenBSD.sh          $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "FreeBSD" ]; then
-    cd release && sh how-to-build/FreeBSD.sh      $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/FreeBSD.sh          $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "NetBSD" ]; then
-    cd release && sh how-to-build/NetBSD.sh       $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/NetBSD.sh           $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "Minix" ]; then
-    cd release && sh how-to-build/minix3.sh       $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/minix3.sh           $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "DragonFly" ]; then
-    cd release && sh how-to-build/DragonFlyBSD.sh $1  > /dev/null && echo "Done.";
+    cd release && sh how-to-build/DragonFlyBSD.sh     $1  > /dev/null && echo "Done.";
 elif [ "$UNAME" = "Haiku" ]; then
-    cd release && sh how-to-build/haiku.sh        $1  > /dev/null && echo "Done.";
+    if $(uname -a | grep -q x86_64); then
+        cd release && sh how-to-build/haiku-x86_64.sh $1  > /dev/null && echo "Done.";
+    else
+        cd release && sh how-to-build/haiku.sh        $1  > /dev/null && echo "Done.";
+    fi
 else
     echo "This platform is not handled by Makefile at the moment.";
     echo "Please build using a script from 'release/how-to-build'.";
