1a00fc2a8e3feb3e2e30c0279975b77581126bc5
octaspire <octaspire@octaspire.com>
2018-07-29 15:44:05 +0300

    WIP

--- dev/doc/book/examples/sh/unixLike.sh
+++ dev/doc/book/examples/sh/unixLike.sh
@@ -1,6 +1,6 @@
 curl -O octaspire.com/dern.tar.bz2
-curl -O https://octaspire.io/dern.tar.bz2.sha512
-sha512sum -c dern.tar.bz2.sha512
+curl -O https://octaspire.io/dern.sha512
+sha512sum -c dern.sha512
 tar jxf dern.tar.bz2
 cd dern
 sh how-to-build/YOUR_PLATFORM_NAME_HERE.XX
