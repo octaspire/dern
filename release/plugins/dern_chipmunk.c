9b89a65ab8e5fece7d77f369da5dd9896ff4d5b1
octaspire <octaspire@octaspire.com>
2018-09-11 14:56:33 +0300

    Add initial version of Chipmunk 2D physics plugin
    * WIP Makefile is modified (and tested) currently only for OpenBSD,
      so plugin will not probably compile in other platforms yet.

---
+++ release/plugins/dern_chipmunk.c
@@ -0,0 +1,118 @@
+/******************************************************************************
+Octaspire Dern - Programming language
+Copyright 2017, 2018 www.octaspire.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+******************************************************************************/
+#include "octaspire-dern-amalgamated.c"
+#include "chipmunk.h"
+
+static char const * const DERN_CHIPMUNK_PLUGIN_NAME = "dern_chipmunk";
+
+void dern_chipmunk_cpSpace_clean_up_callback(void *payload)
+{
+    octaspire_helpers_verify_not_null(payload);
+    cpSpaceFree((cpSpace*)payload);
+}
+
+octaspire_dern_value_t *dern_chipmunk_cpSpaceNew(
+    octaspire_dern_vm_t * const vm,
+    octaspire_dern_value_t * const arguments,
+    octaspire_dern_value_t * const environment)
+{
+    OCTASPIRE_HELPERS_UNUSED_PARAMETER(environment);
+
+    size_t const stackLength = octaspire_dern_vm_get_stack_length(vm);
+
+    size_t const numArgs = octaspire_dern_value_as_vector_get_length(arguments);
+
+    if (numArgs != 0)
+    {
+        octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+        return octaspire_dern_vm_create_new_value_string_format(
+            vm,
+            "Builtin 'chipmunk-cpSpaceNew' expects no arguments. "
+            "%zu arguments were given.",
+            numArgs);
+    }
+
+    cpSpace *space = cpSpaceNew();
+
+    if (!space)
+    {
+        octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+
+        octaspire_dern_value_t * const result =
+            octaspire_dern_vm_create_new_value_string_from_c_string(
+                vm,
+                "Builtin 'chipmunk-cpSpace' failed: cannot create "
+                "new space");
+
+        return result;
+    }
+
+    octaspire_helpers_verify_true(stackLength == octaspire_dern_vm_get_stack_length(vm));
+
+    octaspire_dern_value_t * const result =
+        octaspire_dern_vm_create_new_value_c_data(
+        vm,
+        DERN_CHIPMUNK_PLUGIN_NAME,
+        "cpSpace",
+        "dern_chipmunk_cpSpace_clean_up_callback",
+        "",
+        "",
+        "",
+        false,
+        space);
+
+    return result;
+}
+
+bool dern_chipmunk_init(
+    octaspire_dern_vm_t * const vm,
+    octaspire_dern_environment_t * const targetEnv)
+{
+    octaspire_helpers_verify_true(vm && targetEnv);
+
+    if (!octaspire_dern_vm_create_and_register_new_builtin(
+            vm,
+            "chipmunk-cpSpaceNew",
+            dern_chipmunk_cpSpaceNew,
+            0,
+            "NAME\n"
+            "\tchipmunk-cpSpaceNew\n"
+            "\n"
+            "SYNOPSIS\n"
+            "\t(require 'dern_chipmunk)\n"
+            "\n"
+            "\t(chipmunk-cpSpace) -> cpSpace or error message\n"
+            "\n"
+            "DESCRIPTION\n"
+            "\tCreates and returns a new Chipmunk cpSpace.\n"
+            "\n"
+            "ARGUMENTS\n"
+            "\n"
+            "RETURN VALUE\n"
+            "\tcpSpace to be used with those functions of this library that\n"
+            "\texpect cpSpace argument.\n"
+            "\n"
+            "SEE ALSO\n",
+            false,
+            targetEnv))
+    {
+        return false;
+    }
+
+    return true;
+}
+
