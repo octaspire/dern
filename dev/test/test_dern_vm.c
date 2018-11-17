7f786dd85f75630f52a5588f68abe0f5531ec7f5
octaspire <octaspire@octaspire.com>
2018-11-17 23:09:30 +0200

    WIP

--- dev/test/test_dern_vm.c
+++ dev/test/test_dern_vm.c
@@ -3626,7 +3626,6 @@ TEST octaspire_dern_vm_special_while_with_special_for_value_with_special_while_t
             "      (++ t))))");
 
     ASSERT(evaluatedValue);
-    octaspire_dern_value_print(evaluatedValue, octaspire_dern_vm_get_allocator(vm));
     ASSERT_EQ(OCTASPIRE_DERN_VALUE_TAG_INTEGER, evaluatedValue->typeTag);
     ASSERT_EQ(10,                              evaluatedValue->value.integer);
 
@@ -3636,7 +3635,6 @@ TEST octaspire_dern_vm_special_while_with_special_for_value_with_special_while_t
             "t");
 
     ASSERT(evaluatedValue);
-    octaspire_dern_value_print(evaluatedValue,  octaspire_dern_vm_get_allocator(vm));
     ASSERT_EQ(OCTASPIRE_DERN_VALUE_TAG_INTEGER, evaluatedValue->typeTag);
     ASSERT_EQ(1100,                             evaluatedValue->value.integer);
 
