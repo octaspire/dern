b8a68c5107d53a7f2ca512f68b64caf3914f0c29
octaspire <octaspire@octaspire.com>
2018-12-08 23:29:16 +0200

    Bounce: add more controls, Dern: remove Pygments related things

--- dev/etc/pygments_init.rb
+++
@@ -1,6 +0,0 @@
-# This file is based on documentation from:
-# https://github.com/asciidoctor/asciidoctor.org/blob/master/docs/_includes/src-pygments.adoc
-require 'pygments'
-Pygments.start "#{ENV['HOME']}/hg/pygments-main"
-Pygments::Lexer.create name: 'dern', aliases: ['dern'], filenames: ['*.dern'],
-    mimetypes: ['text/dern', 'application/x-dern']
